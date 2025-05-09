1839f4c5dd59c5f990663fd798866e5aa36fe3fa
Flesh-Gordon <thefleshgordon@gmail.com>
2025-05-09 09:42:42 -0400

    Update reddit_client.py

--- reddit_client.py
+++ reddit_client.py
@@ -7,7 +7,7 @@ load_dotenv()
 reddit = praw.Reddit(
     client_id=os.getenv("REDDIT_CLIENT_ID"),
     client_secret=os.getenv("REDDIT_SECRET"),
-    user_agent="YachtThotBot/1.0",
+    user_agent=os.getenv("USER_AGENT"),
     username=os.getenv("REDDIT_USERNAME"),
     password=os.getenv("REDDIT_PASSWORD"),
 )
