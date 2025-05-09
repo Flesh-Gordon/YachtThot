0a36df7ed56318f647b4c35c56df6c6911aae566
Flesh-Gordon <thefleshgordon@gmail.com>
2025-05-09 11:21:09 -0400

    Update youtube_client.py

--- youtube_client.py
+++ youtube_client.py
@@ -1,28 +1,29 @@
-# youtube_client.py
-
 import os
 from googleapiclient.discovery import build
 from dotenv import load_dotenv
 
 load_dotenv()
-
 YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
 
-def fetch_song_link(query):
-    youtube = build("youtube", "v3", developerKey=YOUTUBE_API_KEY)
-    request = youtube.search().list(
-        part="snippet",
-        q=query,
-        maxResults=1,
-        type="video"
+youtube = build("youtube", "v3", developerKey=YOUTUBE_API_KEY)
+
+def get_video_details(video_id):
+    request = youtube.videos().list(
+        part="snippet,contentDetails",
+        id=video_id
     )
     response = request.execute()
 
-    if response["items"]:
-        video = response["items"][0]
-        video_id = video["id"]["videoId"]
-        title = video["snippet"]["title"]
-        url = f"https://www.youtube.com/watch?v={video_id}"
-        return title, url
+    if not response["items"]:
+        return None
 
-    return None, None
+    item = response["items"][0]
+    title = item["snippet"]["title"]
+    description = item["snippet"]["description"]
+    tags = item["snippet"].get("tags", [])
+
+    return {
+        "title": title,
+        "description": description,
+        "tags": tags
+    }
