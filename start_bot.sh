e225e6d987fac26a46af6b2b6ce922a9169560da
Flesh-Gordon <thefleshgordon@gmail.com>
2025-05-09 12:23:03 -0400

    Update start_bot.sh

--- start_bot.sh
+++ start_bot.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-cd /home/thefleshgordon/reddit_bot
+cd /home/thefleshgordon/reddit_bot || exit
+
+# Optional: load .env manually if needed
+# export $(grep -v '^#' .env | xargs)
+
 source venv/bin/activate
 python3 main.py
